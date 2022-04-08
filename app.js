635758e5a336932e81551722d3a9403e42a7cada
Eğitim <mysql_query@hotmail.com.tr>
2022-04-08 07:29:40 +0300

    login bitti

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
 const http = require('http');
 const socketio = require('socket.io');
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8555'));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8555"));
 
-const server   = http.createServer((req,res)=>{
-    res.end("Server is running....");
+const server = http.createServer((req, res) => {
+    res.end('Server is running...');
 });
 
-server.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
-
-const io = socketio(server,{
-    cors:{
-        origin:'*',
-        methods:['GET'],
+const io = socketio(server, {
+    cors: {
+        origin: '*',
+        methods: 'GET',
     }
 });
 
+io.on('connection', (socket) => {
+    let imzalanacakveri = "Merhaba lütfe bu (" + socket.id + ") veriyi imzalayın";
 
-io.on('connection',(socket)=>{
-    let imzalancakVeri = "Hoş geldiniz lüfen bu veriyi imzalayın : " + socket.id;
-
-
-    socket.on('auth',(data)=>{
-
-        socket.emit('imzala',{
-            message:imzalancakVeri
+    socket.on('login', (data) => {
+        let frontEndAddress = data.address;
+        socket.emit('imzaAt', {
+            veri: imzalanacakveri
         });
     });
 
-    socket.on('imzalandi',(data)=>{
+    socket.on('imzaladim', (data) => {
+        let cozumlenmisimza = web3.eth.accounts.recover(imzalanacakveri, data.imza)
 
-        console.log(data.address);
-        console.log(data.imza);
-
-        let veri = web3.eth.accounts.recover(imzalancakVeri,data.imza);
-        if(veri.toLowerCase() == data.address.toLowerCase()){
-            socket.emit('login',{success:true});
-        }else{
-            console.log("Imza yanlış");
+        if (data.address.toLowerCase() == cozumlenmisimza.toLowerCase()) {
+            socket.emit('imzaCozum', {
+                success: true
+            });
+        } else {
+            socket.emit('imzaCozum', {
+                success: false
+            });
         }
 
-
-
     });
-
 
 
 });
